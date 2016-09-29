35c3086285efb82cdb8e1937bb9fb9fcd070653d
julie1013 <julie1380@verizon.net>
2016-09-29 11:50:23 -0400

    Added create, update, and destroy
    Added create, update, and destroy methods to books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,30 @@ class BooksController < ApplicationController
     render json: Book.find(params[:id])
   end
 
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      render json: @book, status: :created, location: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @book.update(book_params)
+      head :no_content
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @book.destroy
+
+    head :no_content
+  end
+
   def set_book
     @book = Book.find(params[:id])
   end
@@ -17,4 +41,6 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :author)
   end
+
+  private :set_book, :book_params
 end
